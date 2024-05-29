d9ddf1f72ec9e704e97832f0aee1e6ee75e280b3
zhoneyi <144317928+zhoneyi@users.noreply.github.com>
2024-05-29 13:53:35 +0800

    login 頁面更新

--- resources/js/components/Login.vue
+++ resources/js/components/Login.vue
@@ -1,26 +1,57 @@
 <template>
-    <div class="background">
-        <div class="shape"></div>
-        <div class="shape"></div>
-    </div>
-
-    <form @submit.prevent="login">
-        <h3>使用者登入</h3>
-
-        <label for="username">帳號</label>
-        <input v-model="account" type="text" placeholder="請輸入帳號" id="username">
-
-        <label for="password">密碼</label>
-        <input v-model="password" type="password" placeholder="請輸入密碼" id="password">
-
-        <button type="submit" id="btn-login" class="btn btn-outline-light">登入</button>
-        <!--
-        <div class="social">
-            <div class="go"><i class="fab fa-google"></i>  Google</div>
-            <div class="fb"><i class="fab fa-facebook"></i>  Facebook</div>
+    <section class="bg-light py-3 py-md-5">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-12 col-sm-10 col-md-8 col-lg-6 col-xl-5 col-xxl-4">
+                    <div class="card border border-light-subtle rounded-3 shadow-sm">
+                        <div class="card-body p-3 p-md-4 p-xl-5">
+                            <div class="text-center mb-3">
+                                <a href="#!">
+                                    <img src="" alt="Logo" width="175" height="57">
+                                </a>
+                            </div>
+                            <h2 class="fs-6 fw-normal text-center text-secondary mb-4">使用者登入</h2>
+                            <form @submit.prevent="login">
+                                <div class="row gy-2 overflow-hidden">
+                                    <div class="col-12">
+                                        <div class="form-floating mb-3">
+                                            <input v-model="account" type="text" class="form-control" id="username" placeholder="" />
+                                            <label class="form-label" for="username">帳號</label>
+                                        </div>
+                                    </div>
+                                    <div class="col-12">
+                                        <div class="form-floating mb-3">
+                                            <input v-model="password" type="password" class="form-control" id="password" placeholder="" />
+                                            <label class="form-label" for="form2Example2">密碼</label>
+                                        </div>
+                                    </div>
+                                    <div class="col-12">
+                                        <div class="d-flex gap-2 justify-content-between">
+                                            <div class="form-check">
+                                                <input class="form-check-input" type="checkbox" value="" name="rememberMe" id="rememberMe">
+                                                <label class="form-check-label text-secondary" for="rememberMe">
+                                                    保持登入
+                                                </label>
+                                            </div>
+                                            <a href="#!" class="link-primary text-decoration-none">忘記密碼?</a>
+                                        </div>
+                                    </div>
+                                    <div class="col-12">
+                                        <div class="d-grid my-3">
+                                            <button class="btn btn-primary btn-lg" id="btn-login" type="submit">登入</button>
+                                        </div>
+                                    </div>
+                                    <div class="col-12">
+                                        <p class="m-0 text-secondary text-center">還沒有帳號嗎? <a href="#!" class="link-primary text-decoration-none">註冊</a></p>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        -->
-    </form>
+    </section>
 </template>
 
 <script setup>
@@ -48,133 +79,3 @@
         });
     }
 </script>
-
-<!--Stylesheet-->
-<style media="screen" scoped>
-    *,
-    *:before,
-    *:after{
-        padding: 0;
-        margin: 0;
-        box-sizing: border-box;
-    }
-    body{
-        background-color: #080710;
-    }
-    .background{
-        width: 430px;
-        height: 520px;
-        position: absolute;
-        transform: translate(-50%,-50%);
-        left: 50%;
-        top: 50%;
-    }
-    .background .shape{
-        height: 200px;
-        width: 200px;
-        position: absolute;
-        border-radius: 50%;
-    }
-    .shape:first-child{
-        background: linear-gradient(
-            #1845ad,
-            #23a2f6
-        );
-        left: -80px;
-        top: -80px;
-    }
-    .shape:last-child{
-        background: linear-gradient(
-            to right,
-            #ff512f,
-            #f09819
-        );
-        right: -30px;
-        bottom: -80px;
-    }
-    form{
-        height: 520px;
-        width: 400px;
-        background-color: rgba(255,255,255,0.13);
-        position: absolute;
-        transform: translate(-50%,-50%);
-        top: 50%;
-        left: 50%;
-        border-radius: 10px;
-        backdrop-filter: blur(10px);
-        border: 2px solid rgba(255,255,255,0.1);
-        box-shadow: 0 0 40px rgba(8,7,16,0.6);
-        padding: 50px 35px;
-    }
-    form *{
-        font-family: 'Poppins',sans-serif;
-        color: #ffffff;
-        letter-spacing: 0.5px;
-        outline: none;
-        border: none;
-    }
-    form h3{
-        font-size: 32px;
-        font-weight: 500;
-        line-height: 42px;
-        text-align: center;
-    }
-    label{
-        display: block;
-        margin-top: 30px;
-        font-size: 16px;
-        font-weight: 500;
-    }
-    input{
-        display: block;
-        height: 50px;
-        width: 100%;
-        background-color: rgba(255,255,255,0.07);
-        border-radius: 3px;
-        padding: 0 10px;
-        margin-top: 8px;
-        font-size: 14px;
-        font-weight: 300;
-    }
-    ::placeholder{
-        color: #e5e5e5;
-    }
-    button{
-        margin-top: 50px;
-        width: 100%;
-        background-color: #ffffff;
-        color: #080710;
-        padding: 15px 0;
-        font-size: 18px;
-        font-weight: 600;
-        border-radius: 5px;
-        cursor: pointer;
-    }
-    .social{
-      margin-top: 30px;
-      display: flex;
-    }
-    .social div{
-      background: red;
-      width: 150px;
-      border-radius: 3px;
-      padding: 5px 10px 10px 5px;
-      background-color: rgba(255,255,255,0.27);
-      color: #eaf0fb;
-      text-align: center;
-    }
-    .social div:hover{
-      background-color: rgba(255,255,255,0.47);
-    }
-    .social .fb{
-      margin-left: 25px;
-    }
-    .social i{
-      margin-right: 4px;
-    }
-</style>
-
-
-<link rel="preconnect" href="https://fonts.gstatic.com">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;600&display=swap" rel="stylesheet"></link>
