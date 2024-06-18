16282e4bdb1b805262dd104d8eb868c7961eef8d
zhoneyi <144317928+zhoneyi@users.noreply.github.com>
2024-06-18 15:40:33 +0800

    更改名稱

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,13 @@
 import { createApp } from 'vue'
 import router from './router'
-import Content from './components/layouts/Content.vue'
+import content from './components/layouts/Content.vue'
 
 import pinia from '@/stores/index'
 import useStore from '@/stores/store'
 
 const app = createApp({});
 
-app.component("Content", Content);
+app.component("content", content);
 app.use(pinia);
 app.config.globalProperties.$pinia = useStore(); // 定義在 Vue 全域
 
